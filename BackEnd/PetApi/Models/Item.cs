c075bc435228e52d3d6ab00cb91f000ce11713e7
ekhubbell <56080240+ekhubbell@users.noreply.github.com>
2022-11-25 14:27:23 -0500

    Added Backend and Began programming
    Added Backend project
    Added ItemsController
    Added Models Folder
    Added Item class to Models

---
+++ BackEnd/PetApi/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetApi.Models
+{
+    public class Item
+    {
+        [Key]
+        public string Item_ID { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Quantity { get; set; }
+        public string PetType { get; set; }
+
+        public Item(string id, string name, string desc, string quantity, string petType)
+        {
+            Item_ID = id;
+            Name = name;
+            Desc = desc;
+            Quantity = quantity;
+            PetType = petType;
+        }
+        public Item()
+        {
+            Item_ID = "-1";
+        }
+    }
+}
