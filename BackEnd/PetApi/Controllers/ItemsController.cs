935deb8aaaff8e3fb53d2866524edecd416a89cb
ekhubbell <56080240+ekhubbell@users.noreply.github.com>
2022-11-27 18:46:54 -0500

    Updated Items Controller
    Tested and updated Items Controller. It is now functional
    Added Helper class(GatherInfo) to get additional data for the backend and avoid repetition

--- BackEnd/PetApi/Controllers/ItemsController.cs
+++ BackEnd/PetApi/Controllers/ItemsController.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Utilities;
 using PetApi.Models;
+using PetApi.Helpers;
 using System.Xml.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace PetApi.Controllers
 {
@@ -14,22 +16,45 @@ namespace PetApi.Controllers
 
     public class ItemsController : ControllerBase
     {
+        private string tableName = "items";
+        private string primaryKey = "item_Id";
+        private string specialKey = "item_name";
+        private GatherInfo info = new GatherInfo();
 
-        const string CONNSTR = "server=localhost;user=root;database=;port=3306;password=;";
-        
+        private List<string> keys = null;
         // GET: api/<ItemsController>
         [HttpGet]
-        public IEnumerable<Item> Get()
+        public IEnumerable<Item> Get(string searchBy = "noValue", string searchWord = "noValue")
         {
             var items = new List<Item>();
 
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
+            MySqlConnection conn = new MySqlConnection(info.GetConnection());
             try
             {
                 Console.WriteLine("Connecting to database...");
                 conn.Open();
+                string sql = string.Format("SELECT * FROM {1}", tableName);
+                if (!searchBy.Equals("noValue"))
+                {
+                    keys = info.getKeys(tableName);
+                    if (keys.Contains(searchBy))
+                    {
+                        if (searchBy.Equals(specialKey))
+                        {
+                            sql = string.Format("SELECT * FROM {0} WHERE {1} LIKE '%{2}%'", tableName, searchBy, searchWord);
+                        }
+                        else
+                        {
+                            sql = string.Format("SELECT * FROM {2} WHERE {0} = {1}", searchBy, searchWord, tableName);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException(string.Format("{0} is not a valid parameter", searchBy));
+                    }
+                }
+                
 
-                string sql = "SELECT * FROM items";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
@@ -38,7 +63,7 @@ namespace PetApi.Controllers
                 }
                 rdr.Close();
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
@@ -53,13 +78,13 @@ namespace PetApi.Controllers
         {
             Item item = new Item();
 
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
+            MySqlConnection conn = new MySqlConnection(info.GetConnection());
             try
             {
                 Console.WriteLine("Connecting to database...");
                 conn.Open();
 
-                string sql = "SELECT * FROM items WHERE Item_ID = " +id;
+                string sql = string.Format("SELECT * FROM {0} WHERE {2} = {1}", tableName, id, primaryKey);
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
@@ -80,80 +105,17 @@ namespace PetApi.Controllers
 
         }
 
-        // GET api/<ItemsController>/5
-        [HttpGet("{name}")]
-        public IEnumerable<Item> GetByName(string name)
-        {
-            var items = new List<Item>();
-
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
-            try
-            {
-                Console.WriteLine("Connecting to database...");
-                conn.Open();
-
-                string sql = "SELECT * FROM items WHERE item_Name LIKE %\""+ name +"\"%";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    items.Add(new Item(rdr[0].ToString(), rdr[1].ToString(), rdr[2].ToString(), rdr[3].ToString(), rdr[4].ToString(), rdr[5].ToString()));
-                }
-                rdr.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            conn.Close();
-            Console.WriteLine("Done.");
-            return items;
-        }
-
-        [HttpGet("{id}")]
-        public IEnumerable<Item> GetByType(int id)
-        {
-            var items = new List<Item>();
-
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
-            try
-            {
-                Console.WriteLine("Connecting to database...");
-                conn.Open();
-
-                string sql = "SELECT * FROM items WHERE petType = " +id;
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    items.Add(new Item(rdr[0].ToString(), rdr[1].ToString(), rdr[2].ToString(), rdr[3].ToString(), rdr[4].ToString(), rdr[5].ToString()));
-                }
-                rdr.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
-            conn.Close();
-            Console.WriteLine("Done.");
-            return items;
-        }
-
-
-
         // POST api/<ItemsController> this adds new items
         [HttpPost]
-        public void Post([FromBody] string id, [FromBody] string name, [FromBody] string desc, [FromBody] string quant, [FromBody] string petType)
+        public void Post([FromBody] Item item)
         {
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
+            MySqlConnection conn = new MySqlConnection(info.GetConnection());
             try
             {
                 Console.WriteLine("Connecting...");
                 conn.Open();
 
-                string sql = String.Format("insert into items value({0},\"{1}\",\"{2}\",{03},{4})", id, name, desc, quant, petType);
+                string sql = String.Format("insert into {5} value({0},\"{1}\",\"{2}\",{3},{4})", item.Item_ID, item.Name, item.Desc, item.Quantity, item.PetType, tableName);
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 conn.Close();
             }
@@ -164,35 +126,22 @@ namespace PetApi.Controllers
         }
 
         // PUT api/<ItemsController>/5 this is where to update
-        [HttpPut("{id}")]
-        public void PutQuantity(int id, [FromBody] string value)
+        [HttpPut("{Value}")]
+        public void Put(int id, [FromBody] string value, string key)
         {
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
+            MySqlConnection conn = new MySqlConnection(info.GetConnection());
             try
             {
                 Console.WriteLine("Connecting...");
                 conn.Open();
 
-                string sql = String.Format("UPDATE items SET quantity ={0} WHERE itemId = {1}", value, id);
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                conn.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        [HttpPut("{id}")]
-        public void PutPrice(int id, [FromBody] string value)
-        {
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
-            try
-            {
-                Console.WriteLine("Connecting...");
-                conn.Open();
-
-                string sql = String.Format("UPDATE items SET price ={0} WHERE itemId = {1}", value, id);
+                keys = info.getKeys(tableName);
+                if (!keys.Contains(key))
+                {
+                    throw new Exception(string.Format("{0} is not a valid parameter", key));
+                }
+                
+                string sql = String.Format("UPDATE {3} SET {0} ={1} WHERE {4} = {2}", key, value, id, tableName, primaryKey);
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 conn.Close();
             }
@@ -206,13 +155,13 @@ namespace PetApi.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            MySqlConnection conn = new MySqlConnection(CONNSTR);
+            MySqlConnection conn = new MySqlConnection(info.GetConnection());
             try
             {
                 Console.WriteLine("Connecting...");
                 conn.Open();
 
-                string sql = String.Format("DELETE from items SET WHERE itemId = {2}", id);
+                string sql = String.Format("DELETE from {0} SET WHERE {2} = {1}",tableName, id, primaryKey);
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 conn.Close();
             }
---
+++ BackEnd/PetApi/Helpers/GatherInfo.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using PetApi.Models;
+
+namespace PetApi.Helpers
+{
+    public class GatherInfo
+    {
+
+        private const string CONNSTR = "server=localhost;user=root;database=petsupplyplus;port=3306;password=;";
+        private const string DBNAME = "petsupplyplus";
+        public List<string> getKeys(string table)
+        {
+            List<string> keys = new List<string>();
+
+            MySqlConnection conn = new MySqlConnection(CONNSTR);
+            try
+            {
+                Console.WriteLine("Connecting to database...");
+                conn.Open();
+                string sql = string.Format("SELECT COLUMN_NAME  FROM INFORMATION_SCHEMA.COLUMNS  WHERE TABLE_SCHEMA = {0} AND TABLE_NAME = {1};", DBNAME, table);
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    keys.Add(rdr[0].ToString());
+                }
+                rdr.Close();
+
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Done.");
+            return keys;
+        }
+
+        public string GetConnection()
+        {
+            return CONNSTR;
+        }
+
+        public GatherInfo()
+        {
+
+        }
+    }
+}
