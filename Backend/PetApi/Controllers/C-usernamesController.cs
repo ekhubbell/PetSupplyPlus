b64564e7dcbfff49d56991a8f4078ef02c6a5b27
Brian Pagoni <33495607+expequalszero@users.noreply.github.com>
2022-11-28 23:13:57 -0500

    Username Backend controllers and model - done

--- Backend/PetApi/Controllers/C-usernamesController.cs
+++ Backend/PetApi/Controllers/C-usernamesController.cs
@@ -3,6 +3,7 @@ using MySql.Data.MySqlClient;
 using NuGet.Protocol.Plugins;
 using Org.BouncyCastle.Utilities;
 using PetApi.Models;
+using System.Xml.Linq;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -27,7 +28,7 @@ namespace PetApi.Controllers
 
                 Console.WriteLine("Connecting to database...");
                 conn.Open();
-                string sql = "SELECT * FROM C_Username union select * from E_Username;";
+                string sql = "SELECT * FROM C_Username union select * from E_Username order by userid +0 asc;";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
@@ -59,7 +60,7 @@ namespace PetApi.Controllers
                 Console.WriteLine("Connecting to database...");
                 conn.Open();
 
-                string sql = "SELECT * FROM C_Username WHERE Username = " + username;
+                string sql = "SELECT * FROM C_Username WHERE C_Username = '" + username+"'";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
@@ -86,93 +87,124 @@ namespace PetApi.Controllers
                 Console.WriteLine("Connecting to database...");
                 conn.Open();
 
-                string sql = "SELECT * FROM C_Username WHERE Username = " + username + "and password = MD5(" + password+")";
+                string sql = "SELECT * FROM C_Username WHERE c_username = '" + username + "' and password = MD5('" + password + "');";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-                if (rdr.HasRows) { return "URL USER LOGIN"; }
-
-                string sql1 = "SELECT * FROM E_Username WHERE Username = " + username + "and password = MD5(" + password + ")";
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    if (rdr.HasRows)
+                    {
+                        return "URL USER LOGIN";
+                    }
+                }
+                
+                string sql1 = "SELECT * FROM E_Username WHERE username = '" + username + "' and user_password = MD5('" + password + "');";
                 MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
-                MySqlDataReader rdr1 = cmd1.ExecuteReader();
-                if (rdr1.HasRows) { return "URL employee LOGIN"; }
-                else { return "Username or password is incorrect."; }
+                using (MySqlDataReader rdr1 = cmd1.ExecuteReader())
+                {
+                    if (rdr1.HasRows) { return "URL employee LOGIN"; }
+                    else { return "Username or password is incorrect."; }
+                }
+                
+                
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
+                
             }
 
             conn.Close();
             Console.WriteLine("Done.");
             return "error - possibly redirect to url for login error";
         }
-        // PUT api/<C_UsernameController>/5 this is where to update
+
+
+
+        //// PUT api/<C_UsernameController>/5 this is where to update passwords
         [HttpPut("{id}")]
-        public void Put(string userName, [FromBody] string password)
+
+        public void Put([FromBody] C_Usernames info, int id)
         {
             MySqlConnection conn = new MySqlConnection(CONNSTR);
-            Console.WriteLine("Connecting to database...");
-            conn.Open();
-
-            string sql = "SELECT * FROM C_Username WHERE Username = " + userName + "and password = " + password;
-            string sqlupdate1 = "update C_Username set password =" + password;
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            MySqlDataReader rdr = cmd.ExecuteReader();
-            if (rdr.HasRows)
+            
+            try
             {
-                MySqlCommand cmdCus = new MySqlCommand(sqlupdate1, conn);
-                cmdCus.ExecuteNonQuery();
+                Console.WriteLine("Connecting to database...");
+                conn.Open();
 
-                
+                string sql = String.Format("SELECT c_username, password FROM C_Username WHERE  userID= {0}", id);
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                using (MySqlDataReader rdr = cmd.ExecuteReader())
+                {
+
+                    if (rdr.HasRows)
+                    {
+                        rdr.Close();
+                        string sqlupdate = String.Format("Update C_Username set password = md5('{1}') WHERE c_username = '{0}'", info.userName,info.password);
+                        MySqlCommand cmdup = new MySqlCommand(sqlupdate, conn);
+                        cmdup.ExecuteNonQuery();
+
+                    }
+                    else {
+                        Console.WriteLine(sql);
+                    }
+                }
             }
-
-            string sqlEmployee = "SELECT * FROM E_Username WHERE Username = " + userName + "and password = " + password;
-            string sqlupdateEmployee = "update E_Username set password =" + password;
-            MySqlCommand cmd1 = new MySqlCommand(sqlEmployee, conn);
-            MySqlDataReader rdr1 = cmd1.ExecuteReader();
-            if (rdr1.HasRows)
+            catch (Exception e)
             {
-                MySqlCommand cmdEmploy = new MySqlCommand(sqlEmployee, conn);
-                cmdEmploy.ExecuteNonQuery();
+                Console.WriteLine(e.ToString());
+
             }
-
-
-
         }
-        // PUT api/<C_UsernameController>/5 this is where to update
-        [HttpPut("{id}")]
-        public void Put(string userName, [FromBody] string password)
-        {
+
+
+
+
+            // POST api/<C-username Controller> this adds new customers 
+            [HttpPost]
+            public void Post([FromBody] C_Usernames info)
+            {
             MySqlConnection conn = new MySqlConnection(CONNSTR);
-            Console.WriteLine("Connecting to database...");
-            conn.Open();
-
-            string sql = "SELECT * FROM C_Username WHERE Username = " + userName + "and password = " + password;
-            string sqlupdate1 = "update C_Username set password =" + password;
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            MySqlDataReader rdr = cmd.ExecuteReader();
-            if (rdr.HasRows)
+            try
             {
-                MySqlCommand cmdCus = new MySqlCommand(sqlupdate1, conn);
-                cmdCus.ExecuteNonQuery();
+                Console.WriteLine("Connecting...");
+                conn.Open();
 
-
+                string sql = String.Format("insert into C_userName value({0},'{1}',MD5('{2}'))", info.userID,info.userName,info.password);
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
             }
-
-            string sqlEmployee = "SELECT * FROM E_Username WHERE Username = " + userName + "and password = " + password;
-            string sqlupdateEmployee = "update E_Username set password =" + password;
-            MySqlCommand cmd1 = new MySqlCommand(sqlEmployee, conn);
-            MySqlDataReader rdr1 = cmd1.ExecuteReader();
-            if (rdr1.HasRows)
+            catch (Exception e)
             {
-                MySqlCommand cmdEmploy = new MySqlCommand(sqlEmployee, conn);
-                cmdEmploy.ExecuteNonQuery();
+                Console.WriteLine(e.ToString());
             }
-
-
-
         }
 
+
+
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{C_id}")]
+        public void Delete(int C_id)
+        {
+                MySqlConnection conn = new MySqlConnection(CONNSTR);
+                try
+                {
+                    Console.WriteLine("Connecting...");
+                    conn.Open();
+
+                    string sql = String.Format("delete from C_userName where userId = {0}", C_id);
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                conn.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            
+        }
 
 
     }
--- Backend/PetApi/Models/E_usernames.cs
+++ Backend/PetApi/Models/E_usernames.cs
@@ -2,28 +2,28 @@
 
 namespace PetApi.Models
 {
-    public class C_Usernames
+    public class E_Usernames
     {
         internal object? newPassword;
 
         public string userID { get; set; }
         public string userName { get; set; }
         public string password { get; set; }
-       
+        
 
-        public C_Usernames(string userid, string userN, string passwd )
+        public E_Usernames(string userid, string userN, string passwd)
         {
             userID = userid;
             userName = userN;
             password = passwd;
-           
+            
         }
-        public C_Usernames()
+        public E_Usernames()
         {
             userName = "-1";
         }
 
-        public static implicit operator string(C_Usernames v)
+        public static implicit operator string(E_Usernames v)
         {
             throw new NotImplementedException();
         }
